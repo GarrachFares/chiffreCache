1ef268d17a20dea8b9d2c577a3b2df70ee00458c
Garrach <faresgarrach@insat.u-carthage.tn>
2021-03-13 09:02:24 +0100

    uploading_exe1

---
+++ js/jeu.js
@@ -0,0 +1,25 @@
+
+while(confirm("vouler vous lancer le jeu ?")){
+    var niveau = parseInt(prompt("donner le niveau du jeu" , 5) ) ;
+    while(isNaN(niveau) || niveau <= 0){
+        niveau = parseInt(prompt("donner le niveau du jeu" , 5) ) ;
+    }
+    var nbr = Math.ceil(Math.random() * 10) ;
+    var ch = "" ;
+    while(niveau --){
+        var answer = prompt(ch +"il vous rest "+(niveau +1 )+" essai donner votre reponse" ) ;
+        if(answer == nbr){
+            alert("votre reponse est correct  :)  !") ;
+            break ;
+        }else if(answer < nbr){
+            ch = "la reponse est plus grande ,"
+        }else{
+            ch = "la reponse est plus petite ,"
+        }
+        // exit
+    }
+    if(niveau == -1){
+        alert("vous aver perdu ! la reponse est " + nbr) ;
+    }
+    
+}
